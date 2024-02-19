f51d48c7a56e33cc530d89b27485a0cf4bce4e51
Eyal Zusiman <eyal.zusiman@gmail.com>
2024-02-19 20:53:44 +0200

    horde path drawing

--- WallyMapSpinzor2.Raylib/src/ui/ImGuiExtensions.cs
+++ WallyMapSpinzor2.Raylib/src/ui/ImGuiExtensions.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Linq;
 using System.Numerics;
+using System.Runtime.InteropServices;
 using ImGuiNET;
 
 namespace WallyMapSpinzor2.Raylib;
@@ -23,6 +24,24 @@ public static class ImGuiExt
     {
         ImGui.SliderInt(label, ref value, minValue, maxValue);
         return value;
+    }
+
+
+
+    public static uint DragUInt(string label, uint value, uint speed = 1, uint minValue = uint.MinValue, uint maxValue = uint.MaxValue)
+    {
+        //find the IntPtr for the values
+        //there's a simpler way to do this, but it requires unsafe
+        using DisposableHandle valueHandle = DisposableHandle.Alloc(value, GCHandleType.Pinned);
+        nint valuePtr = valueHandle.AddrOfPinnedObject();
+        using DisposableHandle minValueHandle = DisposableHandle.Alloc(minValue, GCHandleType.Pinned);
+        nint minValuePtr = minValueHandle.AddrOfPinnedObject();
+        using DisposableHandle maxValueHandle = DisposableHandle.Alloc(maxValue, GCHandleType.Pinned);
+        nint maxValuePtr = maxValueHandle.AddrOfPinnedObject();
+        //create the drag
+        ImGui.DragScalar(label, ImGuiDataType.U32, valuePtr, speed, minValuePtr, maxValuePtr);
+        //extract the value from the pointer
+        return Marshal.PtrToStructure<uint>(valuePtr);
     }
 
     public static double DragFloat(string label, double value, double speed = 1, double minValue = double.MinValue, double maxValue = double.MaxValue)
@@ -48,5 +67,54 @@ public static class ImGuiExt
             : Enum.GetNames(type);
         ImGui.Combo(label, ref current, allNames, allNames.Length);
         return Enum.GetName(type, current) ?? "";
+    }
+
+    public static T EnumCombo<T>(string label, T currentValue) where T : struct, Enum
+    {
+        string[] names = Enum.GetNames<T>();
+        int valueIndex = Array.FindIndex(names, s => s == Enum.GetName(currentValue));
+        ImGui.Combo(label, ref valueIndex, names, names.Length);
+        return Enum.Parse<T>(names[valueIndex]);
+    }
+
+    public static void WithDisabled(bool disabled, Action a)
+    {
+        if (disabled) ImGui.BeginDisabled();
+        a();
+        if (disabled) ImGui.EndDisabled();
+    }
+
+    //why isn't this part of the STL?
+    private sealed class DisposableHandle : IDisposable
+    {
+        private bool disposedValue;
+        private readonly GCHandle _handle;
+
+        private DisposableHandle(GCHandle handle) { _handle = handle; }
+        public static DisposableHandle Alloc(object? value, GCHandleType type) => new(GCHandle.Alloc(value, type));
+        public nint AddrOfPinnedObject() => _handle.AddrOfPinnedObject();
+
+        private void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    _handle.Free();
+                }
+                disposedValue = true;
+            }
+        }
+
+        ~DisposableHandle()
+        {
+            Dispose(disposing: false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
     }
 }
