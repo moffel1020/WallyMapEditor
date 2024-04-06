a485662647915113f1071839ef6daf8f25c3d5c8
Eyal Zusiman <eyal.zusiman@gmail.com>
2024-04-06 10:39:16 +0300

    add map hot reloading

--- WallyMapSpinzor2.Raylib/Program.cs
+++ WallyMapSpinzor2.Raylib/Program.cs
@@ -1,26 +1,6 @@
-using System.IO;
-using System.Xml.Linq;
-using WallyMapSpinzor2;
-
-static T DeserializeFromPath<T>(string fromPath)
-    where T : IDeserializable, new()
-{
-    XElement element;
-    using (FileStream fromFile = new(fromPath, FileMode.Open, FileAccess.Read))
-    {
-        element = XElement.Load(fromFile);
-    }
-    return element.DeserializeTo<T>();
-}
-
 string brawlPath = args[0];
 string dumpPath = args[1];
 string fileName = args[2];
 
-LevelDesc ld = DeserializeFromPath<LevelDesc>(Path.Combine(dumpPath, "Dynamic", fileName));
-LevelTypes lt = DeserializeFromPath<LevelTypes>(Path.Combine(dumpPath, "Init", "LevelTypes.xml"));
-LevelSetTypes lst = DeserializeFromPath<LevelSetTypes>(Path.Combine(dumpPath, "Game", "LevelSetTypes.xml"));
-IDrawable drawable = new Level(ld, lt, lst);
-
-WallyMapSpinzor2.Raylib.Editor editor = new(brawlPath, drawable);
+WallyMapSpinzor2.Raylib.Editor editor = new(brawlPath, dumpPath, fileName);
 editor.Run();
