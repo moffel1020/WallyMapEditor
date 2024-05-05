be21187548912516eafad7f9f76bc4be2c48290a
AllHailCheese <eyal.zusiman@gmail.com>
2024-05-05 16:10:15 +0300

    prevent giving item spawns negative width or height

--- WallyMapSpinzor2.Raylib/src/Gui/Properties/ItemSpawnProps.cs
+++ WallyMapSpinzor2.Raylib/src/Gui/Properties/ItemSpawnProps.cs
@@ -6,8 +6,8 @@ public partial class PropertiesWindow
     {
         bool propChanged = ImGuiExt.DragFloatHistory($"x##props{i.GetHashCode()}", i.X, (val) => i.X = val, cmd);
         propChanged |= ImGuiExt.DragFloatHistory($"y##props{i.GetHashCode()}", i.Y, (val) => i.Y = val, cmd);
-        propChanged |= ImGuiExt.DragFloatHistory($"w##props{i.GetHashCode()}", i.W, (val) => i.W = val, cmd);
-        propChanged |= ImGuiExt.DragFloatHistory($"h##props{i.GetHashCode()}", i.H, (val) => i.H = val, cmd);
+        propChanged |= ImGuiExt.DragFloatHistory($"w##props{i.GetHashCode()}", i.W, (val) => i.W = val, cmd, minValue: 1);
+        propChanged |= ImGuiExt.DragFloatHistory($"h##props{i.GetHashCode()}", i.H, (val) => i.H = val, cmd, minValue: 1);
         return propChanged;
     }
 }
