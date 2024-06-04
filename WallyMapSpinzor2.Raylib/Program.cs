20d19669b10a2340a45f6e95e938f2d1b9652273
EyalZusiman <eyal.zusiman@gmail.com>
2024-06-04 22:08:24 +0300

    improve the cmdline args by giving them names and allowing only putting some of them

--- WallyMapSpinzor2.Raylib/Program.cs
+++ WallyMapSpinzor2.Raylib/Program.cs
@@ -1,13 +1,28 @@
+using System.Collections.Generic;
 using WallyMapSpinzor2.Raylib;
 
 PathPreferences prefs = PathPreferences.Load();
-if (args.Length >= 4)
+
+Dictionary<string, string> cmdArgs = [];
+foreach (string arg in args)
 {
-    prefs.BrawlhallaPath = args[0];
-    prefs.LevelDescPath = args[1];
-    prefs.LevelTypePath = args[2];
-    prefs.BoneTypesPath = args[3];
+    string[] parts = arg.Split('=', 2);
+    if (parts.Length != 2) continue;
+    cmdArgs[parts[0]] = parts[1];
 }
+
+if (cmdArgs.TryGetValue("--brawlPath", out string? brawlPath))
+    prefs.BrawlhallaPath = brawlPath;
+if (cmdArgs.TryGetValue("--levelDesc", out string? levelDesc))
+    prefs.LevelDescPath = levelDesc;
+if (cmdArgs.TryGetValue("--levelTypes", out string? levelTypes))
+    prefs.LevelTypePath = levelTypes;
+if (cmdArgs.TryGetValue("--boneTypes", out string? boneTypes))
+    prefs.BoneTypesPath = boneTypes;
+if (cmdArgs.TryGetValue("--brawlAir", out string? brawlAir))
+    prefs.BrawlhallaAirPath = brawlAir;
+if (cmdArgs.TryGetValue("--swzKey", out string? swzKey))
+    prefs.DecryptionKey = swzKey;
 
 Editor editor = new(prefs);
 editor.Run();
