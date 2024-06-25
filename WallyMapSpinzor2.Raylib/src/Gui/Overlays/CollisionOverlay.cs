1b2f4a3721537242f6f40645136bd6847bd68237
moffel1020 <moffel1020@gmail.com>
2024-06-25 19:27:06 +0200

    make CollisionOverlay work with collision that are part of a DynamicCollision

--- WallyMapSpinzor2.Raylib/src/Gui/Overlays/CollisionOverlay.cs
+++ WallyMapSpinzor2.Raylib/src/Gui/Overlays/CollisionOverlay.cs
@@ -7,8 +7,12 @@ public class CollisionOverlay(AbstractCollision col) : IOverlay
 
     public bool Update(OverlayData data, CommandHistory cmd)
     {
-        (circle1.X, circle1.Y) = (col.X1, col.Y1);
-        (circle2.X, circle2.Y) = (col.X2, col.Y2);
+        (double offsetX, double offsetY) = (0, 0);        
+        if (col.Parent is not null && data.Context.PlatIDDynamicOffset.TryGetValue(col.Parent.PlatID, out (double, double) dynOffset))
+            (offsetX, offsetY) = (col.Parent.X + dynOffset.Item1, col.Parent.Y + dynOffset.Item2);
+
+        (circle1.X, circle1.Y) = (col.X1 + offsetX, col.Y1 + offsetY);
+        (circle2.X, circle2.Y) = (col.X2 + offsetX, col.Y2 + offsetY);
 
         circle1.Update(data, true);
         circle2.Update(data, !circle1.Dragging);
@@ -18,7 +22,7 @@ public class CollisionOverlay(AbstractCollision col) : IOverlay
             cmd.Add(new PropChangeCommand<(double, double)>(
                 val => (col.X1, col.Y1) = val,
                 (col.X1, col.Y1),
-                (circle1.X, circle1.Y)));
+                (circle1.X - offsetX, circle1.Y - offsetY)));
         }
 
         if (circle2.Dragging)
@@ -26,7 +30,7 @@ public class CollisionOverlay(AbstractCollision col) : IOverlay
             cmd.Add(new PropChangeCommand<(double, double)>(
                 val => (col.X2, col.Y2) = val,
                 (col.X2, col.Y2),
-                (circle2.X, circle2.Y)));
+                (circle2.X - offsetX, circle2.Y - offsetY)));
         }
 
         return circle1.Dragging || circle1.Hovered || circle2.Dragging || circle2.Hovered;
