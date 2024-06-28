31c59db98912ed20f45b2ecccf9416aa9f212205
EyalZusiman <eyal.zusiman@gmail.com>
2024-06-28 22:19:50 +0300

    add anchor overlay

--- WallyMapSpinzor2.Raylib/src/Gui/Overlays/CollisionOverlay.cs
+++ WallyMapSpinzor2.Raylib/src/Gui/Overlays/CollisionOverlay.cs
@@ -4,6 +4,10 @@ public class CollisionOverlay(AbstractCollision col) : IOverlay
 {
     public DragCircle Circle1 { get; set; } = new(col.X1, col.Y1);
     public DragCircle Circle2 { get; set; } = new(col.X2, col.Y2);
+    public DragCircle? Anchor { get; set; } =
+    col.AnchorX is not null && col.AnchorY is not null
+        ? new(col.AnchorX.Value, col.AnchorY.Value)
+        : null;
 
     public bool Update(OverlayData data, CommandHistory cmd)
     {
@@ -13,9 +17,22 @@ public class CollisionOverlay(AbstractCollision col) : IOverlay
 
         (Circle1.X, Circle1.Y) = (col.X1 + offsetX, col.Y1 + offsetY);
         (Circle2.X, Circle2.Y) = (col.X2 + offsetX, col.Y2 + offsetY);
+        if (Anchor is null)
+        {
+            if (col.AnchorX is not null && col.AnchorY is not null)
+                Anchor = new(col.AnchorX.Value, col.AnchorY.Value);
+        }
+        else
+        {
+            if (col.AnchorX is null || col.AnchorY is null)
+                Anchor = null;
+            else
+                (Anchor.X, Anchor.Y) = (col.AnchorX.Value, col.AnchorY.Value);
+        }
 
         Circle1.Update(data, true);
         Circle2.Update(data, !Circle1.Dragging);
+        Anchor?.Update(data, !Circle1.Dragging && !Circle2.Dragging);
 
         if (Circle1.Dragging)
         {
@@ -33,12 +50,24 @@ public class CollisionOverlay(AbstractCollision col) : IOverlay
                 (Circle2.X - offsetX, Circle2.Y - offsetY)));
         }
 
-        return Circle1.Dragging || Circle1.Hovered || Circle2.Dragging || Circle2.Hovered;
+        if (Anchor is not null && Anchor.Dragging)
+        {
+            cmd.Add(new PropChangeCommand<(double, double)>(
+                val => (col.AnchorX, col.AnchorY) = val,
+                (col.AnchorX!.Value, col.AnchorY!.Value),
+                (Anchor.X - offsetX, Anchor.Y - offsetY)));
+        }
+
+        return
+            Circle1.Dragging || Circle1.Hovered ||
+            Circle2.Dragging || Circle2.Hovered ||
+            (Anchor is not null && (Anchor.Dragging || Anchor.Hovered));
     }
 
     public void Draw(OverlayData data)
     {
         Circle1.Draw(data);
         Circle2.Draw(data);
+        Anchor?.Draw(data);
     }
 }
