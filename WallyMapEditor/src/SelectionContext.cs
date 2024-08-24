69c7d17c1b0acda9308c4577012c6effbd28218d
moffel1020 <moffel1020@gmail.com>
2024-08-24 13:20:25 +0200

    add collisions to IsChildOf check

--- WallyMapEditor/src/SelectionContext.cs
+++ WallyMapEditor/src/SelectionContext.cs
@@ -9,6 +9,7 @@ public class SelectionContext
     public bool IsChildOf(object? o) => Object switch
     {
         AbstractAsset a => o == a.Parent || IsChildOf(a.Parent),
+        AbstractCollision c => o == c.Parent,
         AbstractItemSpawn i => o == i.Parent,
         Respawn r => o == r.Parent,
         NavNode n => o == n.Parent,
