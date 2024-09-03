3ea9ed4fe1dbe171424a5fa9995e7ac08a3c088a
moffel1020 <moffel1020@gmail.com>
2024-09-03 22:10:05 +0200

    only read each swz file once when loading

--- WallyMapEditor/src/Importing/Methods/OverridableGameLoad.cs
+++ WallyMapEditor/src/Importing/Methods/OverridableGameLoad.cs
@@ -1,5 +1,8 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using BrawlhallaSwz;
 using WallyMapSpinzor2;
 
 namespace WallyMapEditor;
@@ -40,14 +43,62 @@ public class OverridableGameLoad : ILoadMethod
         string initPath = Path.Combine(BrawlPath, "Init.swz");
 
         LevelDesc ld = LoadFile<LevelDesc>(DescOverride, dynamicPath, "LevelDesc_" + SwzLevelName, DecryptionKey) ?? throw new FileLoadException("Could not load LevelDesc from swz or path");
-        LevelTypes lt = LoadFile<LevelTypes>(TypesOverride, initPath, "LevelTypes.xml", DecryptionKey) ?? throw new FileLoadException("Could not load LevelTypes from swz or path");
-        LevelSetTypes lst = LoadFile<LevelSetTypes>(SetTypesOverride, gamePath, "LevelSetTypes.xml", DecryptionKey) ?? throw new FileLoadException("Could not load LevelSetTypes from swz or path");
-        BoneTypes bt = LoadFile<BoneTypes>(BonesOverride, initPath, "BoneTypes.xml", DecryptionKey) ?? throw new FileLoadException("Could not load BoneTypes from swz or path");
-        string? powerTypesContent = WmeUtils.GetFileInSwzFromPath(gamePath, "powerTypes.csv", DecryptionKey);
-        string[]? pt = powerTypesContent is null ? null
-            : WmeUtils.ParsePowerTypesFromString(powerTypesContent);
+        (LevelTypes lt, BoneTypes bt) = ReadFromInitSwz(initPath);
+        (LevelSetTypes lst, string[]? pt) = ReadFromGameSwz(gamePath);
 
         return new LoadedData(new(ld, lt, lst), bt, pt);
+    }
+
+    private (LevelTypes, BoneTypes) ReadFromInitSwz(string initPath)
+    {
+        BoneTypes? bt = null;
+        LevelTypes? lt = null;
+
+        string[] initSwzFiles = [];
+        if (TypesOverride is null) initSwzFiles = ["LevelTypes.xml"];
+        else lt = WmeUtils.DeserializeFromPath<LevelTypes>(TypesOverride, bhstyle: true);
+
+        if (BonesOverride is null) initSwzFiles = [..initSwzFiles, "BoneTypes.xml"];
+        else bt = WmeUtils.DeserializeFromPath<BoneTypes>(BonesOverride, bhstyle: true);
+
+        Dictionary<string, string> initFiles = ReadFilesFromSwz(initPath, initSwzFiles);
+        lt ??= WmeUtils.DeserializeFromString<LevelTypes>(initFiles["LevelTypes.xml"]);
+        bt ??= WmeUtils.DeserializeFromString<BoneTypes>(initFiles["BoneTypes.xml"]);
+
+        return (lt, bt);
+    }
+
+    private (LevelSetTypes, string[]?) ReadFromGameSwz(string initPath)
+    {
+        LevelSetTypes? lst = null;
+        string[]? pt = null;
+
+        string[] gameSwzFiles = [];
+        if (SetTypesOverride is null) gameSwzFiles = ["LevelSetTypes.xml"];
+        else lst = WmeUtils.DeserializeFromPath<LevelSetTypes>(SetTypesOverride, bhstyle: true);
+
+        if (PowersOverride is null) gameSwzFiles = [..gameSwzFiles, "powerTypes.csv"];
+        else pt = WmeUtils.ParsePowerTypesFromPath(PowersOverride);
+
+        Dictionary<string, string> gameFiles = ReadFilesFromSwz(initPath, gameSwzFiles);
+        lst ??= WmeUtils.DeserializeFromString<LevelSetTypes>(gameFiles["LevelSetTypes.xml"]);
+        pt ??= WmeUtils.ParsePowerTypesFromString(gameFiles["powerTypes.csv"]);
+
+        return (lst, pt);
+    }
+
+    private Dictionary<string, string> ReadFilesFromSwz(string swzPath, string[] filesToSave)
+    {
+        Dictionary<string, string> files = [];
+        foreach (string content in WmeUtils.GetFilesInSwz(swzPath, DecryptionKey))
+        {
+            string swzName = SwzUtils.GetFileName(content);
+            if  (filesToSave.Contains(swzName))
+                files.Add(swzName, content);
+
+            if (files.Count == filesToSave.Length) break;
+        }
+        return files;
     }
 
     private static T? LoadFile<T>(string? overridePath, string swzPath, string swzFile, uint key) where T : IDeserializable, new() =>
