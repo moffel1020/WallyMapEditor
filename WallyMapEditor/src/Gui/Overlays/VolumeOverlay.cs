024c31b248e4b3251478658d696713dd10899cc9
moffel1020 <moffel1020@gmail.com>
2024-10-23 22:19:28 +0200

    round to int in volume overlay

--- WallyMapEditor/src/Gui/Overlays/VolumeOverlay.cs
+++ WallyMapEditor/src/Gui/Overlays/VolumeOverlay.cs
@@ -1,3 +1,4 @@
+using System;
 using WallyMapSpinzor2;
 
 namespace WallyMapEditor;
@@ -14,19 +15,22 @@ public class VolumeOverlay(AbstractVolume volume) : IOverlay
         ResizableBox.Draw(data);
     }
 
+    private static (int, int, int, int) RoundBoundsToInt((double, double, double, double) val) =>
+        ((int)Math.Round(val.Item1), (int)Math.Round(val.Item2), (int)Math.Round(val.Item3), (int)Math.Round(val.Item4));
+
     public bool Update(OverlayData data, CommandHistory cmd)
     {
         ResizableBox.CircleRadius = data.OverlayConfig.RadiusVolumeCorner;
 
         ResizableBox.Update(data, volume.X, volume.Y, volume.W, volume.H);
-        (double x, double y, double w, double h) = ResizableBox.Bounds;
+        (int x, int y, int w, int h) = RoundBoundsToInt(ResizableBox.Bounds);
 
         if (ResizableBox.Resizing)
         {
             cmd.Add(new PropChangeCommand<(int, int, int, int)>(
                 val => (volume.X, volume.Y, volume.W, volume.H) = val,
                 (volume.X, volume.Y, volume.W, volume.H),
-                ((int)x, (int)y, (int)w, (int)h)));
+                (x, y, w, h)));
         }
 
         if (ResizableBox.Moving)
@@ -34,7 +38,7 @@ public class VolumeOverlay(AbstractVolume volume) : IOverlay
             cmd.Add(new PropChangeCommand<(int, int)>(
                 val => (volume.X, volume.Y) = val,
                 (volume.X, volume.Y),
-                ((int)x, (int)y)));
+                (x, y)));
         }
 
         return ResizableBox.Moving || ResizableBox.Resizing;
