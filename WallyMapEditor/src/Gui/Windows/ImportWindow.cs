7a59f657b691f1e90e5a02595951a22ee3b6d465
EyalZusiman <eyal.zusiman@gmail.com>
2024-11-06 00:32:38 +0200

    remove unused imports

--- WallyMapEditor/src/Gui/Windows/ImportWindow.cs
+++ WallyMapEditor/src/Gui/Windows/ImportWindow.cs
@@ -2,7 +2,6 @@ using System;
 using System.IO;
 using System.Threading.Tasks;
 using System.Linq;
-using System.Numerics;
 
 using BrawlhallaSwz;
 using WallyMapEditor.Mod;
--- WallyMapEditor/src/Importing/LoadStressTester.cs
+++ WallyMapEditor/src/Importing/LoadStressTester.cs
@@ -1,7 +1,5 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using BrawlhallaSwz;
 using WallyMapSpinzor2;
 
