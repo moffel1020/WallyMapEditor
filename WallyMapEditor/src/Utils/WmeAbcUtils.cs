ee32ef48a8199f9907249a8714ee457cb9b7a65d
EyalZusiman <eyal.zusiman@gmail.com>
2025-05-09 22:39:39 +0300

    optimize swz key finding by using Spans and rewriting complex Linq

--- WallyMapEditor/src/Utils/WmeAbcUtils.cs
+++ WallyMapEditor/src/Utils/WmeAbcUtils.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using AbcDisassembler;
 using SwfLib;
 using SwfLib.Tags.ActionsTags;
@@ -9,32 +11,65 @@ namespace WallyMapEditor;
 
 public static partial class WmeUtils
 {
-    private static List<int> FindGetlexPositions(CPoolInfo cpool, string lexName, List<Instruction> code) => code
-        .Select((o, i) => new { Item = o, Index = i })
-        .Where(o => o.Item.Name == "getlex" && o.Item.Args[0].Value is INamedMultiname name && cpool.Strings[(int)name.Name] == lexName)
-        .Select(o => o.Index)
-        .ToList();
+    private static List<int> FindGetlexPositions(CPoolInfo cpool, string lexName, List<Instruction> code)
+    {
+        List<int> result = [];
+        for (int i = 0; i < code.Count; ++i)
+        {
+            Instruction instruction = code[i];
+            if (instruction.Name == "getlex" &&
+                instruction.Args[0].Value is INamedMultiname named &&
+                cpool.Strings[(int)named.Name] == lexName)
+            {
+                result.Add(i);
+            }
+        }
+        return result;
+    }
 
-    private static int FindCallpropvoidPos(CPoolInfo cpool, string methodName, List<Instruction> code) => code
-        .FindIndex(i => i.Name == "callpropvoid" && i.Args[0].Value is INamedMultiname named && cpool.Strings[(int)named.Name] == methodName);
+    private static int FindCallpropvoidPos(CPoolInfo cpool, string methodName, ReadOnlySpan<Instruction> code)
+    {
+        for (int i = 0; i < code.Length; ++i)
+        {
+            Instruction instruction = code[i];
+            if (instruction.Name == "callpropvoid" &&
+                instruction.Args[0].Value is INamedMultiname named &&
+                cpool.Strings[(int)named.Name] == methodName)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
 
-    private static uint? FindLastPushuintArg(List<Instruction> ins) => (uint?)ins
-        .LastOrDefault(ins => ins.Name == "pushuint")?.Args[0].Value;
+    private static uint? FindLastPushuintArg(ReadOnlySpan<Instruction> code)
+    {
+        for (int i = code.Length - 1; i >= 0; ++i)
+        {
+            Instruction instruction = code[i];
+            if (instruction.Name == "pushuint")
+                return (uint)instruction.Args[0].Value;
+        }
+        return null;
+    }
 
     public static uint? FindDecryptionKey(AbcFile abc)
     {
         foreach (MethodBodyInfo mb in abc.MethodBodies)
         {
-            List<int> getlexPos = FindGetlexPositions(abc.ConstantPool, "ANE_RawData", mb.Code);
+            ReadOnlySpan<Instruction> instructions = CollectionsMarshal.AsSpan(mb.Code);
 
+            List<int> getlexPos = FindGetlexPositions(abc.ConstantPool, "ANE_RawData", mb.Code);
             for (int i = 0; i < getlexPos.Count; i++)
             {
-                int callpropvoidPos = getlexPos[i] == getlexPos[^1]
-                    ? FindCallpropvoidPos(abc.ConstantPool, "Init", mb.Code[getlexPos[i]..])
-                    : FindCallpropvoidPos(abc.ConstantPool, "Init", mb.Code[getlexPos[i]..getlexPos[i + 1]]);
+                ReadOnlySpan<Instruction> relevantCode = getlexPos[i] == getlexPos[^1]
+                    ? instructions[getlexPos[i]..]
+                    : instructions[getlexPos[i]..getlexPos[i + 1]];
+
+                int callpropvoidPos = FindCallpropvoidPos(abc.ConstantPool, "Init", relevantCode);
 
                 if (callpropvoidPos != -1)
-                    return FindLastPushuintArg(mb.Code[0..callpropvoidPos]);
+                    return FindLastPushuintArg(instructions[0..callpropvoidPos]);
             }
         }
 
@@ -51,6 +86,8 @@ public static partial class WmeUtils
 
     public static uint? FindDecryptionKeyFromPath(string bhairPath)
     {
+        // TODO: rip apart the SwfLib implementation to avoid storing tags we don't need in memory
+
         DoABCDefineTag? tag = GetDoABCDefineTag(bhairPath);
         if (tag is null) return null;
 
