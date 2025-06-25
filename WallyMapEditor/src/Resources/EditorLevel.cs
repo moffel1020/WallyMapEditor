1d7bd90a7d29c11d5efce2b32297b60ff5dbd119
EyalZusiman <eyal.zusiman@gmail.com>
2025-06-25 18:35:07 +0300

    first team collision drawing causing an editor crash

--- WallyMapEditor/src/Resources/EditorLevel.cs
+++ WallyMapEditor/src/Resources/EditorLevel.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Numerics;
 using Raylib_cs;
 using WallyMapSpinzor2;
 
@@ -12,7 +13,7 @@ public sealed class EditorLevel
     public OverlayManager OverlayManager { get; }
     public ILoadMethod? ReloadMethod { get; set; } = null;
 
-    public Camera2D Camera { get; set; } = new();
+    public Camera2D Camera { get; set; } = new(Vector2.Zero, Vector2.Zero, 0, 1);
     public bool DidCameraInit { get; set; } = false;
 
     public EditorLevel(Level level)
